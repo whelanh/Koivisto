b09056e4b28e2d966022bb963a18d85f705a8776
Kim Kahre <kimkahre@outlook.com>
2020-09-22 21:19:42 +0300

    Less Agressive

--- src_files/eval.cpp
+++ src_files/eval.cpp
@@ -695,7 +695,7 @@ bb::Score Evaluator::evaluate(Board* b) {
     res = sumE[0] * (1 - phase) + sumL[0] * (phase);
     res += (b->getActivePlayer() == WHITE ? 15 : -15);
 
-    if (phase>0.9 && b->getPieces()[WHITE_BISHOP] && b->getPieces()[BLACK_BISHOP] && bitCount(WHITE_SQUARES&(b->getPieces()[WHITE_BISHOP]|b->getPieces()[BLACK_BISHOP])) == 1)res = res/2; 
+    if (phase>0.9 && b->getPieces()[WHITE_BISHOP] && b->getPieces()[BLACK_BISHOP] && bitCount(WHITE_SQUARES&(b->getPieces()[WHITE_BISHOP]|b->getPieces()[BLACK_BISHOP])) == 1)res *= 0.75; 
 
     return res;
     // clang-format on
