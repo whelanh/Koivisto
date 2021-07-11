ae6e31774fa18b0bbaccc7759ac4bc5082848243
Kim Kahre <kimkahre@outlook.com>
2021-07-11 12:40:45 +0300

    test
    bench: 7192679

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -1007,7 +1007,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
             // if the depth we are going to search the move at is small enough and the static exchange
             // evaluation for the given move is very negative, dont consider this quiet move as well.
             // ******************************************************************************************************
-            if (moveDepth <= 5 + quiet * 3 && (getCapturedPieceType(m)) < (getMovingPieceType(m))
+            if (!givesCheck && moveDepth <= 5 + quiet * 3 && (getCapturedPieceType(m)) < (getMovingPieceType(m))
                 && b->staticExchangeEvaluation(m) <= (quiet ? -40 * moveDepth : -100 * moveDepth))
                 continue;
         }
